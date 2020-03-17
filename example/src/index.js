65c35065e2feb8bdefb92263103a0c5b9cdba4bf
Douglas Duteil <douglasduteil@gmail.com>
2020-03-17 22:55:07 +0100

    chore: render the example
    Removes `Expected server HTML to contain a matching <h3> in <div>.` warning

--- example/src/index.js
+++ example/src/index.js
@@ -1,13 +1,13 @@
-import React, { Fragment } from "react";
-import { hydrate } from "react-dom";
+import React from "react";
+import { render } from "react-dom";
 
 import France from "../../src";
 
 const Demo = () => (
-  <Fragment>
+  <>
     <h3>Demo react-departements</h3>
     <France departements={[75, 92, 93, 94, 89, 21, 33, 47, 74, "2a", 976]} />
-  </Fragment>
+  </>
 );
 
-hydrate(<Demo />, document.getElementById("root"));
+render(<Demo />, document.getElementById("root"));
