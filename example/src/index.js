86e13b1494dc41c82ade9a4e0185fd808acb0f3d
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2020-04-09 09:19:59 +0200

    feat(deps): upgrade svgmt + tests (#61)

--- example/src/index.js
+++ example/src/index.js
@@ -4,10 +4,10 @@ import { render } from "react-dom";
 import France from "../../src";
 
 const Demo = () => (
-  <>
+  <React.Fragment>
     <h3>Demo react-departements</h3>
     <France departements={[75, 92, 93, 94, 89, 21, 33, 47, 74, "2a", 976]} />
-  </>
+  </React.Fragment>
 );
 
 render(<Demo />, document.getElementById("root"));
